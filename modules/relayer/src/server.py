ba3dd4a07171b9301b47b1dca7a5e5df18d83c6b
Marcos Mota <marcosfmmota@gmail.com>
2019-03-13 20:19:31 -0300

    Inital script using flask-restful

--- modules/relayer/src/server.py
+++ modules/relayer/src/server.py
@@ -1,11 +1,13 @@
 from flask import Flask, request
+from flask_restful import Resource, Api
 import paho.mqtt.client as paho
 import random
 
 app = Flask(__name__)
+api = Api(app)
 
 broker_host    = "broker"
-broker_port      = 1883 
+broker_port    = 1883
 
 broker = paho.Client("publisher{0}".format(random.randint(0,99999999)) )
 broker.connect(broker_host,broker_port)
@@ -14,21 +16,20 @@ broker.connect(broker_host,broker_port)
 def index():
     return 'Harena Logger Relayer'
 
-@app.route('/publish', methods=['POST'])
-def publish():
+class Publisher(Resource):
 
 	# payload = request.args.get('payload', '')
+    def post(self):
+    	message = request.get_json()
+    	print(message)
+    	topic   = message['topic']
+    	payload = message['payload']
 
-	message = request.get_json()
-	print(message)
-	topic   = message['topic']
-	payload = message['payload']
+    	broker.publish(topic,payload)
 
+    	return 'Message published successfully'
 
-	broker.publish(topic,payload) 
-
-	return 'Message published successfully'
-
+api.add_resource(Publisher, '/publish')
 
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", port=8080, debug=True)
