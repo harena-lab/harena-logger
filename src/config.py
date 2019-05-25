e4a781d5dffcafd032df17d5252d90b9135a2d8a
Matheus Mota <smota.matheus@gmail.com>
2019-05-25 19:59:39 -0300

    updating default variables

--- src/config.py
+++ src/config.py
@@ -2,14 +2,17 @@ import os
 
 class Config(object):
 
-    HARENA_LOGGER_BROKER_HOST        = os.environ.get('HARENA_LOGGER_BROKER_HOST',         'localhost')
-    HARENA_LOGGER_BROKER_PORT        = int(os.environ.get('HARENA_LOGGER_BROKER_PORT',           1883))
-
     HARENA_LOGGER_FLASK_HOST         = os.environ.get('HARENA_LOGGER_FLASK_HOST',            '0.0.0.0')
-    HARENA_LOGGER_FLASK_PORT         = int(os.environ.get('HARENA_LOGGER_FLASK_PORT',            5000))
+    HARENA_LOGGER_FLASK_PORT         = int(os.environ.get('HARENA_LOGGER_FLASK_PORT',           10030))
     HARENA_LOGGER_FLASK_DEBUG        = bool(os.environ.get('HARENA_LOGGER_FLASK_DEBUG',         False))
 
     HARENA_LOGGER_MONGODB_HOST       = os.environ.get('HARENA_LOGGER_MONGODB_HOST',        'localhost')
-    HARENA_LOGGER_MONGODB_PORT       = int(os.environ.get('HARENA_LOGGER_MONGODB_PORT',         27017))
+    HARENA_LOGGER_MONGODB_PORT       = int(os.environ.get('HARENA_LOGGER_MONGODB_PORT',         10031))
     HARENA_LOGGER_MONGODB_DB         = os.environ.get('HARENA_LOGGER_MONGODB_DB',      'harena_logger')
     HARENA_LOGGER_MONGODB_COLLECTION = os.environ.get('HARENA_LOGGER_MONGODB_COLLECTION', 'executions')
+
+
+    HARENA_LOGGER_BROKER_HOST        = os.environ.get('HARENA_LOGGER_BROKER_HOST',         'localhost')
+    HARENA_LOGGER_BROKER_PORT        = int(os.environ.get('HARENA_LOGGER_BROKER_PORT',          10032))
+
+
