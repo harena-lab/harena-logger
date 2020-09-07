24bd13eb499fcca18233cdd6ea89c5cbd7a7c39c
Matheus Mota <smota.matheus@gmail.com>
2020-09-07 17:55:03 -0300

    major updates

--- src/config.py
+++ src/config.py
@@ -1,18 +1,32 @@
 import os
 
+
 class Config(object):
 
     HARENA_LOGGER_FLASK_HOST         = os.environ.get('HARENA_LOGGER_FLASK_HOST',            '0.0.0.0')
     HARENA_LOGGER_FLASK_PORT         = int(os.environ.get('HARENA_LOGGER_FLASK_PORT',           10030))
-    HARENA_LOGGER_FLASK_DEBUG        = bool(os.environ.get('HARENA_LOGGER_FLASK_DEBUG',         False))
+    HARENA_LOGGER_FLASK_DEBUG        = bool(os.environ.get('HARENA_LOGGER_FLASK_DEBUG',         True))
 
     HARENA_LOGGER_MONGODB_HOST       = os.environ.get('HARENA_LOGGER_MONGODB_HOST',        'localhost')
     HARENA_LOGGER_MONGODB_PORT       = int(os.environ.get('HARENA_LOGGER_MONGODB_PORT',         10031))
+    HARENA_LOGGER_MONGODB_URL        ="mongodb://{0}:{1}/".format(HARENA_LOGGER_MONGODB_HOST, HARENA_LOGGER_MONGODB_PORT)
     HARENA_LOGGER_MONGODB_DB         = os.environ.get('HARENA_LOGGER_MONGODB_DB',      'harena_logger')
-    HARENA_LOGGER_MONGODB_COLLECTION = os.environ.get('HARENA_LOGGER_MONGODB_COLLECTION', 'executions')
+    HARENA_LOGGER_MONGODB_COLLECTION = os.environ.get('HARENA_LOGGER_MONGODB_COLLECTION', 'event_logs')
+
+    HARENA_LOGGER_KAFKA_BROKERS      = os.environ.get('HARENA_LOGGER_KAFKA_BROKERS', 'kafka1:19092')
+    HARENA_LOGGER_KAFKA_TOPIC        = os.environ.get('HARENA_LOGGER_KAFKA_TOPIC', 'harena-logs')
+    HARENA_LOGGER_INTERVAL_S         = os.environ.get('HARENA_LOGGER_INTERVAL_S', 10)
 
 
-    HARENA_LOGGER_BROKER_HOST        = os.environ.get('HARENA_LOGGER_BROKER_HOST',         'localhost')
-    HARENA_LOGGER_BROKER_PORT        = int(os.environ.get('HARENA_LOGGER_BROKER_PORT',          10032))
+    # LOGGING SETTINGS
+    LOGGING_NAME = os.environ.get('LOGGING_NAME', 'harena-logger')
+    LOGGING_LEVEL = os.environ.get('LOGGING_LEVEL', 'DEBUG')
 
+    LOGGING_STYLES = ('info=blue;'
+                      'warning=green;'
+                      'error=red;'
+                      'critical=red,bold;'
+                      'debug=white')
 
+    LOGGING_FORMAT = ('%(asctime) -19s | %(levelname) -8s | %(threadName) -10s | '
+                      '%(funcName) -16s | %(message)s')
