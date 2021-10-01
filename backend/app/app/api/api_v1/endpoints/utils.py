9c5951f7cabe7e14f3660aa36fd8efc9ebdbf97e
Victor S <victorsaez@stoom.com.br>
2021-10-01 14:25:32 -0300

    adding mongodb and test endpoint

--- backend/app/app/api/api_v1/endpoints/utils.py
+++ backend/app/app/api/api_v1/endpoints/utils.py
@@ -7,9 +7,29 @@ from app import models, schemas
 from app.api import deps
 from app.core.celery_app import celery_app
 from app.utils import send_test_email
+import json
+
+from mongoengine import connect, Document, BooleanField, ObjectIdField
 
 router = APIRouter()
 
+class Test(Document):
+    id = ObjectIdField
+    inserted = BooleanField
+
+
+@router.post("/test-mongodb/", response_model=schemas.Msg, status_code=201)
+def test_celery(
+    msg: schemas.Msg,
+    current_user: models.User = Depends(deps.get_current_active_superuser),
+) -> Any:
+    """
+    Test Celery worker.
+    """
+    connect(db='logger-dev', host='0.0.0.0', port=27017, username=logger, password=harena)
+    print("testing")
+    test_json = json.loads(Test.objects().to_json())
+    return {"test_list": test_json}
 
 @router.post("/test-celery/", response_model=schemas.Msg, status_code=201)
 def test_celery(
