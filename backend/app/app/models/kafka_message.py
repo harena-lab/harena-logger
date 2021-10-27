ca70d828ac210523d3427d1333b73cd51a5dd98d
Victor S <victorsaez@stoom.com.br>
2021-10-27 18:09:35 -0300

    changing message schema

--- backend/app/app/models/kafka_message.py
+++ backend/app/app/models/kafka_message.py
@@ -1,17 +1,23 @@
-from mongoengine import connect, Document, BooleanField, ObjectIdField, StringField, IntField
+from mongoengine import connect, Document, BooleanField, ObjectIdField, StringField, IntField, DateTimeField
 from faust import Record
+from datetime import  datetime
 
 class KafkaMessageDocument(Document):
     _id = ObjectIdField()
-    case = IntField()
-    payload = StringField()
+    session_id = IntField()
+    user_id = IntField()
+    case_id = IntField()
+    knot_id = IntField()
+    timestamp = DateTimeField()
+    hipothesis = StringField()
     origin_ip = StringField()
-    event = StringField()
-
 
 class KafkaMessageRecord(Record, serializer='json'):
-    case: int
-    payload: str
     origin_ip: str
-    event: str
+    session_id: int
+    user_id: int
+    case_id: int
+    knot_id: int
+    timestamp: datetime
+    hipothesis: str
 
--- backend/app/app/schemas/kafka_message.py
+++ backend/app/app/schemas/kafka_message.py
@@ -1,20 +1,27 @@
 from typing import Optional
-
+from datetime import datetime
 from pydantic import BaseModel, EmailStr
 
 
 # Shared properties
 class KafkaMessageBase(BaseModel):
-    case: Optional[int] = None
-    payload: Optional[str] = None
+    session_id: Optional[int] = None
+    user_id: Optional[int] = None
+    case_id: Optional[int] = None
+    knot_id: Optional[int] = None
+    timestamp: Optional[datetime] = None
+    hipothesis: Optional[str] = None
     origin_ip: Optional[str] = None
-    event: Optional[str] = 'default event'
 
 
 # Properties to receive via API on creation
 class KafkaMessageCreate(KafkaMessageBase):
-    case: int
-    payload: str
+    session_id: int
+    user_id: int
+    case_id: int
+    knot_id: int
+    timestamp: datetime
+    hipothesis: str
 
 
 class KafkaMessage(KafkaMessageBase):
