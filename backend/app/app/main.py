acecafebed95545c12a574e567da457d1a549454
Victor S <victorsaez@stoom.com.br>
2021-12-15 19:45:12 -0300

    trying once more

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -1,21 +1,23 @@
 from fastapi import FastAPI
+from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
 
 from app.api.api_v1.api import api_router
 from app.core.config import settings
 
-app = FastAPI(
-    title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
-)
 
-# Set all CORS enabled origins
-if settings.BACKEND_CORS_ORIGINS:
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["*"],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+origins = ["*"]
+
+middleware = [
+    Middleware(CORSMiddleware,
+                allow_origins=origins,
+                allow_credentials=True,
+                allow_methods=["*"],
+                allow_headers=["*"],)
+]
+
+app = FastAPI(
+    title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json", middleware=middleware
+)
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
